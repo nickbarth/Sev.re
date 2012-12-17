de97cc9c5676a2be4af581dbea681b661160c159
Nick Barth <nickbarth@live.com>
2012-12-17 23:11:48 +0000

    escaped package url

--- lib/nginx/dependencies/yum_package.sh
+++ lib/nginx/dependencies/yum_package.sh
@@ -3,7 +3,7 @@ yum_package_install()
   cat >/etc/yum.repos.d/nginx.repo <<CONFIG
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
 gpgcheck=0
 enabled=1
 CONFIG
