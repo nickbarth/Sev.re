caaf8e75ff58d58f64eccfaedfbc968457e060ef
Nick Barth <nickbarth@live.com>
2012-12-18 21:12:35 +0000

    added ruby fix

--- lib/ruby/install.sh
+++ lib/ruby/install.sh
@@ -3,6 +3,6 @@ ruby_install()
 {
   ruby_version=${1:'1.9.3-p327'}
   dependency_install 'ruby' 'yaml' 'packages'
-  source_install "ruby-$ruby_version" \
-                 "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-$ruby_version.tar.gz"
+  source_install "ruby-${ruby_version}" \
+                 "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-${ruby_version}.tar.gz"
 }
