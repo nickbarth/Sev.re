7dea871e6a3da8ec96665e704c92d896a241dd46
Nick Barth <nickbarth@live.com>
2012-12-18 21:18:57 +0000

    updated ruby installer

--- lib/ruby/dependencies/yaml.sh
+++ lib/ruby/dependencies/yaml.sh
@@ -1,6 +1,6 @@
 yaml_install()
 {
-  yaml_version=${1:'0.1.4.'}
+  yaml_version=${1:-'0.1.4.'}
   source_install "yaml-$yaml_version" \
                  "http://pyyaml.org/download/libyaml/yaml-$yaml_version.tar.gz"
 }
--- lib/ruby/install.sh
+++ lib/ruby/install.sh
@@ -1,7 +1,7 @@
 # Ruby Installer
 ruby_install()
 {
-  ruby_version=${1:'1.9.3-p327'}
+  ruby_version=${1:-'1.9.3-p327'}
   dependency_install 'ruby' 'yaml' 'packages'
   source_install "ruby-${ruby_version}" \
                  "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-${ruby_version}.tar.gz"
