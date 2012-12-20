6c00d34708f6ad8437455b55cdd03c86af949562
Nick Barth <nickbarth@live.com>
2012-12-20 20:45:08 +0000

    fixed nodejs installer

--- lib/nodejs/install.sh
+++ lib/nodejs/install.sh
@@ -3,5 +3,5 @@ nodejs_install()
 {
   nodejs_version=${1:-'v0.8.16'}
   binary_install "node-$nodejs_version" \
-                 "http://nodejs.org/dist/nodejs_version/node-$nodejs_version-linux-x64.tar.gz"
+                 "http://nodejs.org/dist/$nodejs_version/node-$nodejs_version-linux-x64.tar.gz"
 }
