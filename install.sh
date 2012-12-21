3040097f572929b8fe5967302041ca4ccf807526
Nick Barth <nickbarth@live.com>
2012-12-20 21:10:58 -0700

    updated installer

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#######################################################
+# Ser.ve - Server Setups Made Simple
+# To learn more about Ser.ve visit http://info.ser.ve/
+#######################################################
+
 # Download and run setup.
 cd /tmp/
-curl -L https://github.com/nickbarth/ServerHelper/tarball/master | tar zx
+curl -L https://github.com/nickbarth/Ser.ve/tarball/master | tar zx
 cd `ls /tmp/* -t1d | head -1`
 source ./setup.sh $@
