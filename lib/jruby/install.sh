77e53953fd3896d882ef41e8c5da2665cec5ff1e
Nick Barth <nickbarth@live.com>
2012-12-21 05:22:56 +0000

    updated jruby installer

--- lib/jruby/install.sh
+++ lib/jruby/install.sh
@@ -3,6 +3,6 @@ jruby_install()
 {
   jruby_version=${1:-'1.7.1'}
   dependency_install 'jruby' 'packages'
-  source_install "jruby-${jruby_version}" \
+  binary_install "jruby-${jruby_version}" \
                  "http://jruby.org.s3.amazonaws.com/downloads/$jruby_version/jruby-bin-$jruby_version.tar.gz"
 }
