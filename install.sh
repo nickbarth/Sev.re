b9d12f79f333da603e0cda23ba23d8f9b6162450
Nick Barth <nickbarth@live.com>
2012-12-30 19:04:40 +0000

    updated installer

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #######################################################
-# Sev.re - Server Setups Made Simple
+# Sev.re - Server setups in one line.
 # To learn more about Sev.re visit http://info.sev.re/
 #######################################################
 
 # Download and run setup.
-cd /tmp/
 curl -L https://github.com/nickbarth/Sev.re/tarball/master | tar zx
-cd `ls /tmp/* -t1d | head -1`
+cd `ls /tmp/* -t1d | grep Sev.re | head -1`
 source ./setup.sh $@
